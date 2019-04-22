c5f4fcb96a208f44479e78736c8ef7655f8ebc49
MinSeok Kim <myminseok@gmail.com>
2019-04-22 16:02:23 +0900

    init

---
+++ download-product.sh
@@ -0,0 +1,1 @@
+fly -t demo sp -p download-product -c download-product.yml -l ./download-product-params.yml
---
+++ install-products.sh
@@ -0,0 +1,1 @@
+fly -t demo sp -p install-products -c install-products.yml -l ./install-products-params.yml
